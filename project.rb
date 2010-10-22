842b44879e6674c3deff1ccf0190574edea6ddb1
Akira Hayakawa <ruby.wktk@gmail.com>
2010-10-22 17:26:51 +0900

    compiled in 64 bits and gtest for .cu succeeded

--- project.rb
+++ project.rb
@@ -2,21 +2,28 @@
 THRUST_HOME = "#{ENV["HOME"]}/local/thrust"
 THRUST_INCLUDE = THRUST_HOME
 
-GTEST_HOME = "#{ENV["HOME"]}/local/cunit/cunit-2.1-2"
+CUNIT_HOME = "#{ENV["HOME"]}/local/cunit/cunit-2.1-2"
+CUNIT_LIB = [CUNIT_HOME, "lib"].join "/"
+CUNIT_INCLUDE = [CUNIT_HOME, "include"].join "/"
+
+GTEST_HOME = "#{ENV["HOME"]}/local/gtest/gtest-1.5.0"
 GTEST_LIB = [GTEST_HOME, "lib"].join "/"
-GTEST_INCLUDE = [GTEST_HOME, "include"]. join "/"
+GTEST_INCLUDE = [GTEST_HOME, "include"].join "/"
+
+CUDA_HOME = "/usr/local/cuda"
+CUDA_LIB = [CUDA_HOME, "lib"].join "/"
 
 thisdir = File.dirname(__FILE__)
 LIBPATH = thisdir
 VERSION = "0.0.0"
 DEVELOPER = ["Akira Hayakawa <ruby.wktk@gmail.com>"]
-COMPILER = "nvcc"
+COMPILER = "nvcc -m64"
 #CC = [COMPILER, LIBPATH, THRUST_INCLUDE].join " -I "
 CC = COMPILER
 testcc = CC
-testcc = [testcc, CUNIT_INCLUDE].join " -I"
-testcc = [testcc, CUNIT_LIB].join " -L"
-testcc = [testcc, "cunit"].join " -l"
+testcc = [testcc, CUNIT_INCLUDE, GTEST_INCLUDE].join " -I"
+testcc = [testcc, CUDA_LIB, CUNIT_LIB, GTEST_LIB].join " -L"
+testcc = [testcc, "cunit", "gtest"].join " -l"
 TESTCC = testcc
 
 #p CC
