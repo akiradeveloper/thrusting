59acea7e23f01c88b361b395ce9f86c209b420ed
Akira Hayakawa <ruby.wktk@gmail.com>
2010-10-22 16:57:51 +0900

    add gtest test

--- project.rb
+++ project.rb
@@ -2,7 +2,7 @@
 THRUST_HOME = "#{ENV["HOME"]}/local/thrust"
 THRUST_INCLUDE = THRUST_HOME
 
-GTEST_HOME = "#{ENV["HOME"]}/local/gtest/gtest-1.5.0"
+GTEST_HOME = "#{ENV["HOME"]}/local/cunit/cunit-2.1-2"
 GTEST_LIB = [GTEST_HOME, "lib"].join "/"
 GTEST_INCLUDE = [GTEST_HOME, "include"]. join "/"
 
@@ -11,4 +11,13 @@ LIBPATH = thisdir
 VERSION = "0.0.0"
 DEVELOPER = ["Akira Hayakawa <ruby.wktk@gmail.com>"]
 COMPILER = "nvcc"
-CC = [COMPILER, LIBPATH, THRUST_LIB].join " -I "
+#CC = [COMPILER, LIBPATH, THRUST_INCLUDE].join " -I "
+CC = COMPILER
+testcc = CC
+testcc = [testcc, CUNIT_INCLUDE].join " -I"
+testcc = [testcc, CUNIT_LIB].join " -L"
+testcc = [testcc, "cunit"].join " -l"
+TESTCC = testcc
+
+#p CC
+p TESTCC
--- testing/compile.rb
+++ testing/compile.rb
@@ -1,6 +1,5 @@
 thisdir = File.dirname(__FILE__)
 require [thisdir, "..", "project"].join "/"
-TESTCC = CC
 
 def compile(src, bin)
   system "#{TESTCC} -o #{bin} #{src}"  
@@ -13,7 +12,7 @@ end
 if __FILE__ == $0
   src = ARGV[0]
   if File.extname(src) == ".cu"
-    # ruby compile.rb hoge.cu
+    # ruby compile.rb hoge.cpp
     bin = File.basename(src, ".cu") + ".bin"
     compile(src, bin) 
     p "-----COMPILATION FINISHED-----"
