8cf97fb8de9d0f6d3ed19592645f72230dcf73cc
Akira Hayakawa <ruby.wktk@gmail.com>
2010-10-25 16:53:16 +0900

    compilation error report in os << (os, tuple)

--- testing/test.cu
+++ testing/test.cu
@@ -2,10 +2,15 @@
 #include <string>
 #include <iostream>
 
+namespace {
+  using namespace thrusting;
+}
+
 template <typename A>
 A func(A x, A y) { return x + y; }
 
 int main(void){
+  thrusting::int3 x;
   func(10,20);
   return 0;
 }
--- testing/test_tuple.cu
+++ testing/test_tuple.cu
@@ -2,22 +2,18 @@
 #include <thrusting/thrusting.h>
 #include <iostream>
 
-// namespace thrusting { 
-
-// copied from tuple_operator.h
-// and succeeded compilation why?
-template<typename T0, typename T1>
-std::ostream &operator<<(std::ostream &os, const thrust::tuple<T0, T1> &x){
-  std::stringstream ss;
-  ss << "(" << 
-thrust::get<0>(x)
- << ", " << 
-thrust::get<1>(x)
- << ")";
-  os << ss.str();
-  return os;
-}
-// } // end thrusting
+// letting this function in std namespace and compilation scceeded, why?
+//template<typename T0, typename T1>
+//std::ostream &operator<<(std::ostream &os, const thrust::tuple<T0, T1> &x){
+//  std::stringstream ss;
+//  ss << "(" << 
+//thrust::get<0>(x)
+// << ", " << 
+//thrust::get<1>(x)
+// << ")";
+//  os << ss.str();
+//  return os;
+//}
 
 TEST(TupleTest, Plus){
   using namespace thrusting;
