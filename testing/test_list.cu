4d57730bf140bb9d4bb6dc00576282cf88b08c02
Akira Hayakawa <ruby.wktk@gmail.com>
2010-10-31 17:53:40 +0900

    with ADL in thrusting::detail space, compiled

--- testing/test_list.cu
+++ testing/test_list.cu
@@ -6,6 +6,26 @@
 
 using namespace thrusting::op::list;
 
+namespace thrusting {
+namespace detail {
+
+template<typename A>
+bool operator==(const thrusting::detail::list<A> &xs, const detail::list<A> &ys){
+//  if(xs.length() != ys.length()){ return false; }
+//  return thrust::equal(xs.head(), xs.head()+xs.length(), ys.head());
+  return true;
+}
+
+template<typename A>
+bool operator!=(const thrusting::detail::list<A> &xs, const detail::list<A> &ys){
+  return true;
+}
+
+}
+}
+
+// using namespace thrusting::detail;
+
 TEST(List, Ostream){
   int xs[] = {1,2}; THRUSTING_VECTOR<int> d_xs(xs, xs+2); 
   std::cout << thrusting::make_list(d_xs) << std::endl;
--- thrusting/detail/list/def_list.h
+++ thrusting/detail/list/def_list.h
@@ -15,12 +15,12 @@ struct list {
   A head() const {
     return _head;
   }
-  bool operator==(const list<A> &ys) const {
-    return true;
-  }
-  bool operator!=(const list<A> &ys) const {
-    return true;
-  }
+//  bool operator==(const list<A> &ys) const {
+//    return true;
+//  }
+//  bool operator!=(const list<A> &ys) const {
+//    return true;
+//  }
 };
 
 } // end detail
