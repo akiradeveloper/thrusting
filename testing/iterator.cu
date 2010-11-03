064e09c3a9fd5da18acb7e716016eff0d6ace14a
Akira Hayakawa <ruby.wktk@gmail.com>
2010-11-03 10:43:25 +0900

    add test for iterator_value_of

--- testing/iterator.cu
+++ testing/iterator.cu
@@ -8,3 +8,8 @@ TEST(Iterator, IteratorValueAt){
   EXPECT_EQ(1, thrusting::iterator_value_at(0, xs.begin()));
   EXPECT_EQ(2, thrusting::iterator_value_at(1, xs.begin()));
 }
+
+TEST(Iterator, IteratorValueOf){
+  int _xs[] = {1,2}; THRUSTING_VECTOR<int> xs(_xs, _xs+2);
+  EXPECT_EQ(1, thrusting::iterator_value_of(xs.begin()));
+}
