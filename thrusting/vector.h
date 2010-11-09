08b55c5b6361d5651c8454fc0500f9c1a519e8ab
Akira Hayakawa <ruby.wktk@gmail.com>
2010-11-09 14:20:06 +0900

    modified and add document. started to be used in bphcuda

--- thrusting/vector.h
+++ thrusting/vector.h
@@ -3,6 +3,25 @@
 #include <thrust/host_vector.h>
 #include <thrust/device_vector.h>
 
+/*
+  Akira Hayakawa 2010 10/9
+  Here we are using Macro.
+  This is little bit periling
+  because it aborts type checking.
+  Using type constructor will enable type checking however,
+  the client code will be unnatural as of
+  
+  thrusting<int>::vector _xs;
+  
+  althought we hope to be writing as
+  
+  thrusting::vector<int> _xs;
+
+  This will be possible in next C++0x but,
+  nvcc does not support or will not support forever.
+  After all we decided to use Macro.
+*/
+
 #ifdef THRUSTING_USE_DEVICE_VECTOR
   #define THRUSTING_VECTOR thrust::device_vector 
 #else
