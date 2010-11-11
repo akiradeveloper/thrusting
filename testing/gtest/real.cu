73064d31e769744104ecd6f19cf78f9923775f95
Akira Hayakawa <ruby.wktk@gmail.com>
2010-11-11 14:31:02 +0900

    real.cu is modified as not using make_realN

--- testing/gtest/real.cu
+++ testing/gtest/real.cu
@@ -5,19 +5,21 @@
 #include <gtest/gtest.h>
 
 namespace {
+  using thrusting::real;
+  using thrusting::real2;
   using namespace thrusting::op;
 }
 
 TEST(real, create){
-  thrusting::real x = 1.0;
-  EXPECT_EQ(1.0, x);
+  real x = 1.0;
+  EXPECT_EQ(real(1.0), x);
 }
 
-TEST(real, tuple){
-  thrusting::real2 x = thrusting::make_tuple2<thrusting::real>(1.0, 2.0);
-  EXPECT_EQ(thrusting::make_tuple2(1.0,2.0), x);
+TEST(real, tuple_create){
+  real2 x = real2(1.0, 2.0);
+  EXPECT_EQ(thrusting::make_tuple2<real>(1.0, 2.0), x);
 }
 
-TEST(real, arithmatic){
-  EXPECT_EQ(thrusting::make_tuple2<thrusting::real>(2.0, 4.0), 2 * thrusting::make_tuple2<thrusting::real>(1.0, 2.0));
+TEST(real, simple_arithmatic){
+  EXPECT_EQ(real2(2.0, 4.0), 2 * real2(1.0, 2.0));
 }
