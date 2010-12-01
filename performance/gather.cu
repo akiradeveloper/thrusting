49c4b7dbdcc1e8b6e319ed3ccf7e20a2d4908ee3
Akira Hayakawa <ruby.wktk@gmail.com>
2010-12-02 01:07:08 +0900

    performance test working now with cudaThreadSynchronize function called

--- performance/gather.cu
+++ performance/gather.cu
@@ -1,55 +1,70 @@
 #include <thrusting/vector.h>
 #include <thrusting/algorithm/gather.h>
 #include <thrusting/time.h>
+#include <thrusting/timer.h>
 #include <thrusting/list.h>
 
 #include <thrust/sequence.h>
-#include <thrust/reverse.h>
+
+#include <algorithm>
 
 namespace {
   using namespace thrusting;
 }
 
 int main(void){
-  size_t N = 1000000;
+  size_t N = 10000000;
 
-  vector<int>::type value(N);
-  thrust::sequence(value.begin(), value.end());
+  thrust::host_vector<int> _value(N);
+  thrust::sequence(_value.begin(), _value.end());
 
-  vector<int>::type idx(N);
-  thrust::sequence(idx.begin(), idx.end());
-  thrust::reverse(idx.begin(), idx.end());
-  
-  thrusting::stopwatch sw("thrusting scatter"); 
+  thrust::host_vector<int> _idx(N);
+  thrust::sequence(_idx.begin(), _idx.end());
 
-  for(int i=0; i<10; ++i){
+  thrusting::stopwatch sw("thrusting"); 
+
+  for(int i=0; i<1; ++i){
+    std::random_shuffle(_value.begin(), _value.end());
+    vector<int>::type value(_value.begin(), _value.end());
+
+    std::random_shuffle(_idx.begin(), _idx.end());
+    vector<int>::type idx(_idx.begin(), _idx.end());
+
+    cudaThreadSynchronize();
     sw.begin();
     thrusting::gather(
       idx.begin(),
       idx.end(),
       value.begin(),
       value.begin()); // in-place
+    cudaThreadSynchronize();
     sw.end();
   }
    
   sw.show();
 
-  thrusting::stopwatch sw2("thrust::scatter");     
+  thrusting::stopwatch sw2("thrust");     
   vector<int>::type output(N);     
   
-  for(int i=0; i<10; ++i){
+  for(int i=0; i<1; ++i){
+    std::random_shuffle(_value.begin(), _value.end());
+    vector<int>::type value(_value.begin(), _value.end());
+
+    std::random_shuffle(_idx.begin(), _idx.end());
+    vector<int>::type idx(_idx.begin(), _idx.end());
+
+    cudaThreadSynchronize();
     sw2.begin();
     thrust::gather(
       idx.begin(),
       idx.end(),
       value.begin(),
       output.begin());
+    cudaThreadSynchronize();
     sw2.end();
   }
   
   sw2.show();
-  
-  std::cout << make_list(10, output.begin()) << std::endl;
   
   return 0;
 }
--- performance/scatter.cu
+++ performance/scatter.cu
@@ -2,9 +2,9 @@
 #include <thrusting/algorithm/scatter.h>
 #include <thrusting/real.h>
 #include <thrusting/time.h>
+#include <thrusting/timer.h>
 
 #include <thrust/sequence.h>
-#include <thrust/reverse.h>
 
 #include <algorithm>
 
@@ -13,7 +13,7 @@ namespace {
 }
 
 int main(void){
-  size_t N = 1000000;
+  size_t N = 10000000;
 
   thrust::host_vector<int> _value(N);
   thrust::sequence(_value.begin(), _value.end());
@@ -23,20 +23,23 @@ int main(void){
   
   thrusting::stopwatch sw("thrusting"); 
 
-  for(int i=0; i<3; ++i){
+  for(int i=0; i<1; ++i){
     std::random_shuffle(_value.begin(), _value.end());
     vector<int>::type value(_value.begin(), _value.end());
 
     std::random_shuffle(_idx.begin(), _idx.end());
     vector<int>::type idx(_idx.begin(), _idx.end());
 
+    cudaThreadSynchronize();
     sw.begin();
+
     thrusting::scatter(
       value.begin(),
       value.end(),
       idx.begin(),
       value.begin()); // in-place
     
+    cudaThreadSynchronize();
     sw.end();
   }
    
@@ -45,13 +48,14 @@ int main(void){
   thrusting::stopwatch sw2("thrust");     
   vector<int>::type output(N);     
   
-  for(int i=0; i<10; ++i){
+  for(int i=0; i<1; ++i){
     std::random_shuffle(_value.begin(), _value.end());
     vector<int>::type value(_value.begin(), _value.end());
 
     std::random_shuffle(_idx.begin(), _idx.end());
     vector<int>::type idx(_idx.begin(), _idx.end());
 
+    cudaThreadSynchronize();
     sw2.begin();
     thrust::scatter(
       value.begin(),
@@ -59,11 +63,7 @@ int main(void){
       idx.begin(),
       output.begin());
 
-    // not needed though
-    cudaEvent_t stop;
-    cudaEventCreate( &stop );
-    cudaEventSynchronize( stop );
-
+    cudaThreadSynchronize();
     sw2.end();
   }
   
