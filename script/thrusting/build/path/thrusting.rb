dd698f13d685a52654234fdd8185471f6b86ef54
Akira Hayakawa <ruby.wktk@gmail.com>
2010-12-11 00:18:32 +0900

    ALL TEST PASSED. gather all scripts under script/ dir

--- script/thrusting/build/path/thrusting.rb
+++ script/thrusting/build/path/thrusting.rb
@@ -1,6 +1,6 @@
 def use_thrusting(cc)
   thisdir = File.expand_path File.dirname __FILE__ 
-  libpath = [thisdir, "..", ".."].join "/"
+  libpath = [thisdir, "..", "..", "..", ".."].join "/"
   libpath = File.expand_path libpath
   cc += " -I #{libpath}"
 end
