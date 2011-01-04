debee06dadcbe4d114c4334e08b780f483d7e860
Akira Hayakawa <ruby.wktk@gmail.com>
2011-01-04 18:44:57 +0900

    succeeded accesssing constant memory

--- bug/constant_memory/main.h
+++ bug/constant_memory/main.h
@@ -2,12 +2,50 @@
 
 #include <iostream>
 
-__device__ __constant__ int x = 2;
+/*
+  Initialization by this kind works
+*/
+__constant__ int x = 100;
+__constant__ int xs[2] = {10,20};
 
 int main(void){
+  
+  /*
+    Initialization by cudaMemcpyToSymbol works
+  */
+//  int xx[2] = {1,2};
+//  cudaMemcpyToSymbol(x, xx, 2*sizeof(int), 0, cudaMemcpyHostToDevice);
+  
+  /*
+    memory space on the host.
+  */
+  int tmps[1];
 
-  int y = x+1;
-  std::cout << y << std::endl; 
+  /*
+    by default,
+    cpy from device to host is choosen.
+    guess it is possible to cpy from device to device (either on global or device code).
+  */
+  cudaMemcpyFromSymbol(tmps, xs, sizeof(int), sizeof(int)*1);
+  /*
+    can be print out fine.
+    cout << x[0] directly without copying data on the constant memory to the host.
+    std::cout can not interpret the data
+    and more badly x[i] can be compiled because the syntax is correct thought it will not work fine.
+    
+    x is a pointer on the device space but compiler take it as one on the host space.
+    the reason x[0] is 0 and x[10] is 0 is the space on the host is not initalized and occasionally allocated.
+
+    some example like
+    http://tech.ckme.co.jp/cuda_const.shtml
+    access constant memory by [i] acccessor but this might be correct because
+    the function is global which works on the device.
+  */
+  std::cout << tmps[0] << std::endl;
+
+  int tmp;
+  cudaMemcpyFromSymbol(&tmp, x, sizeof(int));
+  std::cout << tmp << std::endl;
 
   return 0;
 }
